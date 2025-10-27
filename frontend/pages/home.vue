ccb624fc4c366e09546e6eb0d19ba260bcab3b45
AlanACanoSanchez <a.cano.sanchez.tech@gmail.com>
2025-10-27 11:14:59 -0500

    frontend:pantalla de home y sidebar realizado

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -1,6 +1,20 @@
 <template>
-  <section class="min-h-screen flex flex-col items-center justify-center bg-gray-50">
-    <h1 class="text-4xl font-semibold text-gray-800">Home</h1>
-    <p class="mt-2 text-gray-500">Bienvenido a la página principal</p>
-  </section>
+  <div class="min-h-screen flex bg-gray-50">
+    <!-- Sidebar fijo -->
+    <Sidebar />
+
+    <!-- Contenedor principal -->
+    <div class="flex flex-col flex-1 min-h-screen ml-64 transition-all duration-300">
+      <Header />
+
+
+      <FooterBar />
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import Sidebar from '~/components/Layout/Sidebar.vue'
+import Header from '~/components/Layout/Header.vue'
+import FooterBar from '~/components/Layout/FooterBar.vue'
+</script>
